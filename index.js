3c4893e19dbc8ac5ff4e0095dc2db94292a7498b
LeoMortari <leo.mortari.forinn@gmail.com>
2022-01-18 19:39:29 -0300

    #02 - Adiciona função de Pause e Resume

--- index.js
+++ index.js
@@ -29,16 +29,22 @@ client.on("message", async (msg) => {
 
   //entrar em um servidor
   if (msg.content === prefixo + "entre") {
+    if (!msg.member.voice.channel) {
+      msg.channel.send("Você precisa estar em um canal de voz");
+      return;
+    }
     servidores.server.connection = await msg.member.voice.channel.join();
   }
 
   //Comando de sair
   if (msg.content === prefixo + "sair") {
-    servidores.server.connection = await msg.member.voice.channel.leave();
+    msg.member.voice.channel.leave();
+    servidores.server.connection = null;
+    servidores.server.dispatcher = null;
   }
 
   //comando de dar play em música
-  if (msg.content.includes("play" && prefixo + "p")) {
+  if (msg.content.includes("play")) {
     if (!msg.member.voice.channel) {
       msg.channel.send("Você precisa estar em um canal de voz");
       return;
@@ -46,12 +52,24 @@ client.on("message", async (msg) => {
     servidores.server.connection = await msg.member.voice.channel.join();
     const { content } = msg;
 
-    const video = content.split(" ")[1];
-    try {
-      servidores.server.connection.play(ytdl(video, ytdlOptions));
-    } catch (error) {
-      console.log(error);
+    const link = content.split(" ")[1];
+    if (ytdl.validateURL(link)) {
+      servidores.server.dispatcher = servidores.server.connection.play(
+        ytdl(link, ytdlOptions)
+      );
+    } else {
+      msg.channel.send("Insira um link válido");
     }
+  }
+
+  //Pausar stream
+  if (msg.content === prefixo + "pause") {
+    servidores.server.dispatcher.pause();
+  }
+
+  //Resumir stream
+  if (msg.content === prefixo + "resume") {
+    servidores.server.dispatcher.resume();
   }
 });
 
